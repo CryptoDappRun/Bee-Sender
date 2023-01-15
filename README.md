e5f7bc79d3ffa33e35ce3afd5564fd2b6f56b1e7
CryptoDappRun <122711721+CryptoDappRun@users.noreply.github.com>
2023-01-15 11:54:05 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,54 @@
+# Bee-Sender-v2
+Bee Sender v2-ERC20 Token Airdrop Multi Sender.Packer your multi transfer in one transaction and save your cost.<br>
+
+``Batch Send Token``<br>
+``Batch Send ETH/BNB/Matic...``<br>
+Current support Blockchain networks:<br>
+Ethereum,Binance,Huobi,Fantom,Polygon and Ropsten testnetwork.<br><br>
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/1.png" width="25" height="25" alt="eth"> 
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/56.png" width="25" height="25" alt="bnb">
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/250.png" width="25" height="25" alt="ftm">
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/137.png" width="25" height="25" alt="matic"> 
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/43114.png" width="25" height="25" alt="matic">
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/25.png" width="25" height="25" alt="matic">
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/13381.png" width="25" height="25" alt="matic">
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/img/534.png" width="25" height="25" alt="matic">
+<br>
+V2 add features:<br>
+1.Sending tokens with decimal points is now supported,like:1.01,2.3,33 is accept.<br>
+2.Supports importing addresses and sending quantities using CSV files.<br>
+3.If you want to fork Bee Sender to earn cryptocurrency, version 2 removes the fee limit, and now you can customize the user fee. High or low is up to you.<br>
+4.upgrade GUI.<br>
+5.add Ethereum network.<br>
+
+Send addresses once:<br>
+1-500<br>
+
+<img src="https://github.com/AlgoCryptoDapp/Bee-Sender/blob/main/screen.png" alt="erc20-tokens-multi-sender">
+
+Site1:
+https://AlgoCryptoDapp.github.io/Bee-Sender/
+
+Site2:
+https://dapp404.com/Bee-Sender
+
+
+# Need Earn cryptocurrency?
+Open js/config.js<br>
+Change the address to your own address,customize fee you will recieve.<br>
+
+
+# Deploy to github page
+https://github.com/AlgoCryptoDapp/Page<br>
+
+or you can download source ,and put to any web server.then start earn cryptocurrency!<br>
+
+# You can deployed this source to any server and change the brand name without my approve.
+
+<br>
+Update:<br>
+Version 2.1:Fix bug,add multi language.<br>
+ 
+ 
+ 
+
---
+++ js/contract.js
@@ -0,0 +1,246 @@
+var SmartContractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address payable[]",
+				"name": "addrs",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "amounts",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "address",
+				"name": "_token",
+				"type": "address"
+			},
+			{
+				"internalType": "address payable",
+				"name": "_Referrer",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_ReferrerFee",
+				"type": "uint256"
+			}
+		],
+		"name": "multiSender",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bool",
+				"name": "_AllowReffer",
+				"type": "bool"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_fee",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_minReferrerFee",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_maxReferrerFee",
+				"type": "uint256"
+			}
+		],
+		"name": "SetFee",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_Amount",
+				"type": "uint256"
+			}
+		],
+		"name": "TransferETH",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_Owner",
+				"type": "address"
+			}
+		],
+		"name": "TransferOwner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_tokenAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address payable",
+				"name": "_receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_Amount",
+				"type": "uint256"
+			}
+		],
+		"name": "TT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_fee",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_minReferrerFee",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_maxReferrerFee",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_AddressLimit",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "_AllowReffer",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	},
+	{
+		"inputs": [],
+		"name": "AddressLimit",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "AllowReffer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "fee",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lock",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "maxReferrerFee",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "minReferrerFee",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+var SmartContractAddress = "0xEBC485c1d0F4B214884faa44C8d086509e7363B7"; 
+
